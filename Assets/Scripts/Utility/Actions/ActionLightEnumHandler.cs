f0a1c803c6bf2e27519eb829825215b231a79c44
Justin Perri <justin.perri@nbcuni.com>
2025-12-03 16:49:42 -0800

    light switch functionality

--- Assets/Scripts/Utility/Actions/ActionLightEnumHandler.cs
+++ Assets/Scripts/Utility/Actions/ActionLightEnumHandler.cs
@@ -8,20 +8,44 @@ namespace Assets.Scripts.Utility.Actions
     internal class ActionLightEnumHandler: MonoBehaviour
     {
         [SerializeField] private ActionBoolLightEnumScriptable actionBoolLightEnumScriptable;
-        [SerializeField] private Light2D light2d;
-        [SerializeField] private LightSourceEnum sourceOfLight, offLight;
-        [SerializeField] private float onLightIntensity;
+        [SerializeField] private Light2D[] lightsOnWtihEnum, lightsOffwithEnum;
+        [SerializeField] private LightSourceEnum sourceLightEnum;
+        [SerializeField] private bool sourceOnByDefault;
 
-        private void Start() => actionBoolLightEnumScriptable.AddAction(HandleLightChange);
+        private bool isSourceOn;
+
+        private void Start()
+        {
+            actionBoolLightEnumScriptable.AddAction(HandleLightChange);
+            isSourceOn = sourceOnByDefault;
+        }
 
         private void HandleLightChange(bool isOff, LightSourceEnum lightSource) 
         {
-            if (sourceOfLight == lightSource && isOff)
-                light2d.intensity = 0f;
-            else
-                light2d.intensity = onLightIntensity;
+            if (sourceLightEnum == lightSource)
+                SwitchLights(isOff);
+        }
+
+        private void SwitchLights(bool isOff)
+        {
+            // Turn off lights that should be on
+            foreach (var lightOn in lightsOnWtihEnum)
+                lightOn.enabled = isOff ? false : true;
+
+            // Turn on lights that should be off
+            foreach (var lightOff in lightsOffwithEnum)
+                lightOff.enabled = isOff ? true : false;
         }
 
         private void OnDisable() => actionBoolLightEnumScriptable.RemoveAction(HandleLightChange);
+
+
+        [ContextMenu("Trigger TestToggleLightSwitchOn")]
+        private void TestToggleLightSwitchOn()
+        {
+            isSourceOn = !isSourceOn;
+            HandleLightChange(isSourceOn ? false : true, sourceLightEnum);
+            
+        }
     }
 }
