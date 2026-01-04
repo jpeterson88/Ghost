f02391f900d2690d2a108df0eed33db91cdbb1cf
Justin Perri <justin.perri@nbcuni.com>
2026-01-04 12:36:47 -0800

    Added curtain shake sfx

--- Assets/Scripts/Utility/DashInteractions/CurtainDashInteraction.cs
+++ Assets/Scripts/Utility/DashInteractions/CurtainDashInteraction.cs
@@ -1,4 +1,5 @@
-﻿using Spine;
+﻿using Assets.Scripts.Audio;
+using Spine;
 using UnityEngine;
 
 namespace Assets.Scripts.Utility.DashInteractions
@@ -6,13 +7,15 @@ namespace Assets.Scripts.Utility.DashInteractions
     internal class CurtainDashInteraction : MonoBehaviour, IHandleDashInteraction
     {
         [SerializeField] private Animator animator;
+        [SerializeField] private CachedAudioController audioController;
         [SerializeField] private string whooshLeft, whooshRight, whooshOutwards;
 
         [ContextMenu("Trigger Enable fluctuation")]
         public void TriggerDashInteraction() => HandleDashInteraction(Vector2.left);
         public void HandleDashInteraction(Vector2 dashDirection)
         {
-            if(dashDirection.x < 0)
+            audioController.PlayOneShot();
+            if (dashDirection.x < 0)
                 animator.SetTrigger(whooshLeft);
             else if(dashDirection.x > 0)
                 animator.SetTrigger(whooshRight);
